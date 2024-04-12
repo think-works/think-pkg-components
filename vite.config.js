a230a3b3d1d11097741b846befe097977e0b425e
pfdgithub <pfdgithub@users.noreply.github.com>
2024-04-12 18:13:10 +0800

    feat: 块分析

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import cProcess from "child_process";
 import path from "path";
 import process from "process";
+// import { visualizer } from "rollup-plugin-visualizer";
 import { fileURLToPath } from "url";
 import { defineConfig, loadEnv } from "vite";
 import checker from "vite-plugin-checker";
@@ -140,6 +141,7 @@ export default defineConfig(({ mode }) => {
           "aria-hidden": true,
         },
       }),
+      // visualizer(),
     ],
   };
 });
