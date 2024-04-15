e71e848088e854a640e2893b6dd694959aaf6221
pfdgithub <pfdgithub@users.noreply.github.com>
2024-04-15 16:32:07 +0800

    fix: ref 透传

--- src/components/Resizing/index.tsx
+++ src/components/Resizing/index.tsx
@@ -11,7 +11,7 @@ import {
 import { useThrottle } from "@/hooks";
 import stl from "./index.module.less";
 
-export type ResizingProps = HTMLAttributes<HTMLDivElement> & {
+export type ResizingProps = Omit<HTMLAttributes<HTMLDivElement>, "onResize"> & {
   /** 防抖毫秒时间间隔 */
   throttle?: number;
   /** 将子标签脱离文档流以避免撑开所在容器 */
@@ -23,96 +23,95 @@ export type ResizingProps = HTMLAttributes<HTMLDivElement> & {
 /**
  * 可尺寸变更容器
  */
-export const Resizing = (
-  props: ResizingProps,
-  ref: ForwardedRef<HTMLDivElement>,
-) => {
-  const {
-    className,
-    throttle = 100,
-    detachFlow = true,
-    onResize,
-    ...rest
-  } = props || {};
+export const Resizing = forwardRef(
+  (props: ResizingProps, ref: ForwardedRef<HTMLDivElement>) => {
+    const {
+      className,
+      throttle = 100,
+      detachFlow = true,
+      onResize,
+      ...rest
+    } = props || {};
 
-  const refObserver = useRef<ResizeObserver>();
-  const refContainer = useRef<HTMLDivElement>(null);
+    const refObserver = useRef<ResizeObserver>();
+    const refContainer = useRef<HTMLDivElement>(null);
 
-  useImperativeHandle(ref, () => refContainer.current as HTMLDivElement, []);
+    useImperativeHandle(ref, () => refContainer.current as HTMLDivElement, []);
 
-  const handleContainerResize = useCallback(
-    (entries: ResizeObserverEntry[]) => {
-      const entry = entries[0];
+    const handleContainerResize = useCallback(
+      (entries: ResizeObserverEntry[]) => {
+        const entry = entries[0];
 
-      if (!onResize || !entry) {
+        if (!onResize || !entry) {
+          return;
+        }
+
+        if (entry.contentBoxSize?.[0]) {
+          const { blockSize, inlineSize } = entry.contentBoxSize[0];
+          onResize(inlineSize, blockSize);
+          return;
+        }
+
+        const { width, height } = entry.contentRect;
+        onResize(width, height);
+      },
+      [onResize],
+    );
+
+    const handleWindowResize = useCallback(() => {
+      if (!onResize) {
         return;
       }
 
-      if (entry.contentBoxSize?.[0]) {
-        const { blockSize, inlineSize } = entry.contentBoxSize[0];
-        onResize(inlineSize, blockSize);
-        return;
-      }
-
-      const { width, height } = entry.contentRect;
+      const { width, height } = refContainer.current!.getBoundingClientRect();
       onResize(width, height);
-    },
-    [onResize],
-  );
+    }, [onResize]);
 
-  const handleWindowResize = useCallback(() => {
-    if (!onResize) {
-      return;
-    }
+    const _handleContainerResize = useThrottle(handleContainerResize, throttle);
+    const _handleWindowResize = useThrottle(handleWindowResize, throttle);
 
-    const { width, height } = refContainer.current!.getBoundingClientRect();
-    onResize(width, height);
-  }, [onResize]);
+    const addListener = useCallback(() => {
+      if (window.ResizeObserver) {
+        refObserver.current = new ResizeObserver(_handleContainerResize);
+        refObserver.current.observe(refContainer.current!);
+      } else {
+        window.addEventListener("resize", _handleWindowResize);
+      }
+    }, [_handleContainerResize, _handleWindowResize]);
 
-  const _handleContainerResize = useThrottle(handleContainerResize, throttle);
-  const _handleWindowResize = useThrottle(handleWindowResize, throttle);
+    const removeListener = useCallback(() => {
+      _handleContainerResize.cancel();
+      _handleWindowResize.cancel();
 
-  const addListener = useCallback(() => {
-    if (window.ResizeObserver) {
-      refObserver.current = new ResizeObserver(_handleContainerResize);
-      refObserver.current.observe(refContainer.current!);
-    } else {
-      window.addEventListener("resize", _handleWindowResize);
-    }
-  }, [_handleContainerResize, _handleWindowResize]);
+      if (window.ResizeObserver) {
+        refObserver.current?.disconnect();
+      } else {
+        window.removeEventListener("resize", _handleWindowResize);
+      }
+    }, [_handleContainerResize, _handleWindowResize]);
 
-  const removeListener = useCallback(() => {
-    _handleContainerResize.cancel();
-    _handleWindowResize.cancel();
+    useEffect(() => {
+      addListener();
 
-    if (window.ResizeObserver) {
-      refObserver.current?.disconnect();
-    } else {
-      window.removeEventListener("resize", _handleWindowResize);
-    }
-  }, [_handleContainerResize, _handleWindowResize]);
+      return () => {
+        removeListener();
+      };
+    }, [addListener, removeListener]);
 
-  useEffect(() => {
-    addListener();
+    return (
+      <div
+        className={cls(
+          stl.resizing,
+          {
+            [stl.detachFlow]: detachFlow,
+          },
+          className,
+        )}
+        ref={refContainer}
+        {...rest}
+      />
+    );
+  },
+);
 
-    return () => {
-      removeListener();
-    };
-  }, [addListener, removeListener]);
-
-  return (
-    <div
-      className={cls(
-        stl.resizing,
-        {
-          [stl.detachFlow]: detachFlow,
-        },
-        className,
-      )}
-      ref={refContainer}
-      {...rest}
-    />
-  );
-};
-
-export default forwardRef(Resizing);
+export default Resizing;
