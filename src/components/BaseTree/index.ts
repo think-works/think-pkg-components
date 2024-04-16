5a70e44402379c3c2d999f0c7e3f04900aedbb92
sallyfafafa <944719344@qq.com>
2024-04-16 15:22:45 +0800

    fix: remove tree icon

--- src/components/BaseTree/index.ts
+++ src/components/BaseTree/index.ts
@@ -1,5 +1,4 @@
 export { default as BaseTree, kRootNode as BaseTreeKRootNode } from "./base";
 
-export { default as BaseTreeIcon } from "./icon";
 export * from "./base/types";
 export type * from "./base";
