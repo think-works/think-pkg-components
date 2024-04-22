d3266882c83b3074db0d13718081e13da048f1e3
pfdgithub <pfdgithub@users.noreply.github.com>
2024-04-22 22:11:55 +0800

    fix: 更简化的布局

--- src/components/LayoutSimple/index.tsx
+++ src/components/LayoutSimple/index.tsx
@@ -5,16 +5,6 @@ import stl from "./index.module.less";
 export type LayoutSimpleProps = {
   className?: Argument;
   style?: React.CSSProperties;
-  /** 头部分割线 */
-  divider?: boolean;
-  /** 内容区域紧贴头部 */
-  clingContent?: boolean;
-  /**
-   * 内容区域高度自适应。
-   * 若内容高度低于所在 flex 容器剩余空间则自动撑满，若超过则出现滚动条。
-   * 要求内容区域所在 flex 容器必须指定高度，或者其父容器也是 flex 容器。
-   */
-  overflowContent?: boolean;
   title?: React.ReactNode;
   extend?: React.ReactNode;
   children?: React.ReactNode;
@@ -32,18 +22,8 @@ export type LayoutSimpleProps = {
  * 简单布局
  */
 export const LayoutSimple = (props: LayoutSimpleProps) => {
-  const {
-    className,
-    style,
-    divider,
-    clingContent,
-    overflowContent,
-    title,
-    extend,
-    children,
-    classNames,
-    styles,
-  } = props || {};
+  const { className, style, title, extend, children, classNames, styles } =
+    props || {};
 
   return (
     <div className={cls(stl.layoutSimple, className)} style={style}>
@@ -52,22 +32,11 @@ export const LayoutSimple = (props: LayoutSimpleProps) => {
           className={cls(stl.head, classNames?.head)}
           style={styles?.head}
           size="middle"
-          divider={divider}
           title={title}
           extend={extend}
         />
       ) : null}
-      <div
-        className={cls(
-          stl.body,
-          {
-            [stl.cling]: clingContent,
-            [stl.overflow]: overflowContent,
-          },
-          classNames?.body,
-        )}
-        style={styles?.body}
-      >
+      <div className={cls(stl.body, classNames?.body)} style={styles?.body}>
         {children}
       </div>
     </div>
