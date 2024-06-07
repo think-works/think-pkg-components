37799bd7779a1b1c8d1acb9d83c2c7873ad74d9b
pfdgithub <pfdgithub@users.noreply.github.com>
2024-06-07 22:28:00 +0800

    fix: 尝试增加 forceKey 已避免模块加载过快时，调用方未触发状态更新。

--- src/hooks/useDynamicImport.ts
+++ src/hooks/useDynamicImport.ts
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useReducer, useRef, useState } from "react";
 
 /**
  * 异步加载模块，如：
@@ -24,22 +24,28 @@ import { useEffect, useRef, useState } from "react";
 export const useDynamicImport = <Module = any>(
   dynamicImport: Promise<Module>,
 ) => {
-  // 维护 loading 状态，主要是为了在模块加载后触发组件更新
+  // 维护 forceKey 和 loading 状态，主要是为了在模块加载后触发组件更新
+  const [forceKey, forceUpdate] = useReducer((x: number) => x + 1, 0);
   const [loading, setLoading] = useState(false);
   const refModule = useRef<Module>();
 
   useEffect(() => {
+    forceUpdate();
     setLoading(true);
+
     dynamicImport
       .then((module) => {
         refModule.current = module;
       })
       .finally(() => {
+        forceUpdate();
         setLoading(false);
       });
   }, [dynamicImport]);
 
   return {
+    /** 强制刷新 */
+    forceKey,
     /** 加载中 */
     loading,
     /** 已加载模块 */
