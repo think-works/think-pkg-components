f105839c51927ddc2c77af6ff52f663a7dfc356b
pfdgithub <pfdgithub@users.noreply.github.com>
2024-06-20 19:49:32 +0800

    feat: 增加数据脱敏支持

--- src/utils/crypto.ts
+++ src/utils/crypto.ts
@@ -45,3 +45,86 @@ export const stringHash = (str: string) => {
    * signed int to an unsigned by doing an unsigned bitshift. */
   return hash >>> 0;
 };
+
+/**
+ * 数据脱敏
+ * phone: 12345678901 => 123****8901
+ * identity: 123456789012345678 => 1234**********5678
+ * email: 12345@678.90 => 12****@***.90
+ */
+export const dataMasking = (
+  text: string,
+  options?: {
+    format?: "phone" | "identity" | "email";
+    start?: number;
+    end?: number;
+    mask?: number;
+    char?: string;
+  },
+): string => {
+  const { format, start, end, mask, char = "*" } = options || {};
+
+  let _start = start ?? 1;
+  let _end = end ?? (text.length > 2 ? 1 : 0);
+  let _mask = mask;
+
+  if (format === "phone") {
+    // 处理手机号
+    _start = start ?? 3;
+    _end = end ?? 4;
+  } else if (format === "identity") {
+    // 处理身份证
+    _start = start ?? 4;
+    _end = end ?? 4;
+  } else if (format === "email") {
+    // 处理邮箱
+    const atIdx = text.indexOf("@");
+    if (atIdx !== -1) {
+      // 根据 @ 分割前缀和后缀
+      const prefixText = text.slice(0, atIdx);
+      const suffixText = text.slice(atIdx + 1);
+
+      // 保留第一个 . 之后的字符
+      const dotIdx = suffixText.lastIndexOf(".");
+      const dotLen = dotIdx !== -1 ? suffixText.length - dotIdx : undefined;
+
+      _start = start ?? 2;
+      _end = end ?? dotLen ?? 2;
+      _mask = mask ? mask / 2 : mask;
+
+      // 递归处理前缀和后缀
+      const prefixMask = dataMasking(prefixText, {
+        start: _start,
+        end: 0,
+        mask: _mask,
+        char,
+      });
+      const suffixMask = dataMasking(suffixText, {
+        start: 0,
+        end: _end,
+        mask: _mask,
+        char,
+      });
+
+      return prefixMask + "@" + suffixMask;
+    }
+  }
+
+  let restText = text;
+  let startText = "";
+  let endText = "";
+  let maskText = "";
+
+  // 明文字符 + 脱敏字符
+  startText = restText.slice(0, _start);
+  restText = restText.slice(_start);
+
+  // 脱敏字符 + 明文字符
+  endText = restText.slice(restText.length - _end);
+  restText = restText.slice(0, restText.length - _end);
+
+  // 脱敏字符
+  maskText = char.repeat(_mask ?? restText.length);
+
+  return startText + maskText + endText;
+};
