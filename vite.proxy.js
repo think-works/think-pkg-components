1f2f944274f348a24920f52578ed2e813c42c7d5
pfdgithub <pfdgithub@users.noreply.github.com>
2025-01-16 16:46:18 +0800

    fix: 同步打包配置

--- vite.proxy.js
+++ vite.proxy.js
@@ -1,28 +1,71 @@
-const commonOptions = {
-  changeOrigin: true, // host 请求头
-  cookieDomainRewrite: "", // cookie 所属域
-  // headers: {
-  //   Origin: proxyTarget, // https://github.com/http-party/node-http-proxy/pull/1130
-  //   Connection: "keep-alive", // https://stackoverflow.com/a/53499411
-  // },
+/**
+ * @typedef {import('vite').ProxyOptions} ProxyOptions
+ */
+
+/**
+ * 获取通用选项
+ * @param {ProxyOptions} options
+ * @returns {ProxyOptions}
+ */
+const getCommonOptions = (options) => {
+  const { target, headers, ...rest } = options || {};
+
+  return {
+    target,
+    changeOrigin: true, // host 请求头
+    cookieDomainRewrite: "", // cookie 所属域
+    headers: {
+      referer: target, // referer 检查
+      ...(headers || {}),
+
+      /** Grafana 需要配置 Origin 和 Connection */
+      // Origin: target, // https://github.com/http-party/node-http-proxy/pull/1130
+      // Connection: "keep-alive", // https://stackoverflow.com/a/53499411
+    },
+    ...rest,
+  };
 };
 
+/**
+ * 获取代理规则
+ * @param {string[]} proxyPaths
+ * @param {ProxyOptions} options
+ * @returns {Object.<string, ProxyOptions>}
+ */
 const getProxyRules = (proxyPaths, options) => {
-  return proxyPaths.reduce((rules, path) => {
-    rules[path] = { ...options };
+  return (proxyPaths || []).reduce((rules, path) => {
+    rules[path] = { ...(options || {}) };
     return rules;
   }, {});
 };
 
+/**
+ * 默认的代理路径
+ */
 const fullServiceTarget = "https://dev.xincetest.com:10443";
 
-export default ({ apiBase, proxyTarget }) => ({
-  ...getProxyRules([`/login`, `/meta`, `/quality/api`], {
-    ...commonOptions,
-    target: fullServiceTarget,
-  }),
-  [`${apiBase}api`]: {
-    ...commonOptions,
-    target: proxyTarget || fullServiceTarget,
-  },
-});
+/**
+ * 获取代理配置
+ * @param {Object} config
+ * @param {string} config.apiBase
+ * @param {string} config.proxyTarget
+ * @param {string} config.customPrefix
+ * @returns {Object.<string, ProxyOptions>}
+ */
+export default (config) => {
+  const { apiBase = "/", proxyTarget = "", customPrefix = "" } = config || {};
+
+  return {
+    ...getProxyRules(
+      [
+        `${customPrefix}/login`,
+        `${customPrefix}/meta`,
+        `${customPrefix}/quality/api`,
+      ],
+      getCommonOptions({ target: fullServiceTarget }),
+    ),
+    [`${apiBase}api`]: getCommonOptions({
+      target: proxyTarget || fullServiceTarget,
+    }),
+  };
+};
