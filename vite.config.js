a2d77e2c71e6754f0632cde1ef4eb4d4d55381d3
pfdgithub <pfdgithub@users.noreply.github.com>
2025-07-02 19:10:27 +0800

    fix: 废弃警告

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import process from "process";
 import { fileURLToPath } from "url";
 import { defineConfig, loadEnv } from "vite";
 import checker from "vite-plugin-checker";
-import mockDevServer from "vite-plugin-mock-dev-server";
+import { mockDevServerPlugin } from "vite-plugin-mock-dev-server";
 import svgr from "@svgr/rollup";
 import react from "@vitejs/plugin-react";
 import pkg from "./package.json";
@@ -80,7 +80,7 @@ export default defineConfig(({ mode }) => {
     },
     plugins: [
       react(),
-      mockEnable ? mockDevServer() : undefined,
+      mockEnable ? mockDevServerPlugin() : undefined,
       checker({
         typescript: true,
         eslint: {
