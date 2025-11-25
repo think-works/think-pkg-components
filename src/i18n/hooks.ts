835a074073e97a5eec1303d56e9fdcbf710bcc62
pfdgithub <pfdgithub@users.noreply.github.com>
2025-11-25 19:11:37 +0800

    feat: 获取组件库的本地化资源

--- src/i18n/hooks.ts
+++ src/i18n/hooks.ts
@@ -6,6 +6,30 @@ import { ObjectPaths } from "@/utils/types";
 import { defaultLanguage, defaultLocale, getLocale, Locale } from "./index";
 
 /**
+ * 获取组件库的本地化资源
+ */
+export const getComponentsLocale = async (language?: string) => {
+  const innerLang = language || defaultLanguage;
+  const innerLocale = await getLocale(innerLang);
+
+  const innerFindLocaleText = (
+    paths: ObjectPaths<Locale>,
+    vars?: Record<string, any>,
+  ) => findLocaleText(innerLocale, paths, vars);
+
+  return {
+    /** 预期使用的语言 */
+    lang: innerLang,
+    /** 实际使用的本地化资源 */
+    locale: innerLocale,
+    /** 查找本地化资源文本 */
+    findLocaleText: innerFindLocaleText,
+    /** 替换文本变量 */
+    replaceTextVars: replaceTextVars,
+  };
+};
+
+/**
  * 使用组件库的本地化资源
  */
 export const useComponentsLocale = () => {
