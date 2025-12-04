fb75cbe80bae3012f95c767cbe16e51b20ad871d
pfdgithub <pfdgithub@users.noreply.github.com>
2025-12-04 21:05:03 +0800

    chore: uuid

--- src/utils/cryptos.ts
+++ src/utils/cryptos.ts
@@ -8,23 +8,22 @@
 export const uuid4 = () => {
   const crypto = window.crypto;
 
-  if (crypto && crypto.randomUUID) {
+  if (crypto?.randomUUID) {
     return crypto.randomUUID();
   }
 
-  if (crypto && crypto.getRandomValues) {
-    return `${1e7}-${1e3}-${4e3}-${8e3}-${1e11}`.replace(/[018]/g, (c: any) =>
-      (
-        c ^
-        (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))
-      ).toString(16),
-    );
+  const template = "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx";
+
+  if (crypto?.getRandomValues) {
+    return template.replace(/[xy]/g, (c) => {
+      const r = crypto.getRandomValues(new Uint8Array(1))[0] & 0xf;
+      return (c === "x" ? r : (r & 0x3) | 0x8).toString(16);
+    });
   }
 
-  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, (c) => {
-    const r = (Math.random() * 16) | 0,
-      v = c === "x" ? r : (r & 0x3) | 0x8;
-    return v.toString(16);
+  return template.replace(/[xy]/g, (c) => {
+    const r = (Math.random() * 16) | 0;
+    return (c === "x" ? r : (r & 0x3) | 0x8).toString(16);
   });
 };
 
